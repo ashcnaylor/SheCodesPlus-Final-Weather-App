a91eff94e435cdde0cf8c63e1644f1abc9e345a7
ashcnaylor <112088184+ashcnaylor@users.noreply.github.com>
2022-09-06 10:47:42 -0400

    adding and debugging forecast

--- src/script.js
+++ src/script.js
@@ -34,10 +34,63 @@ let time = now.toLocaleTimeString("en-US", {
 let currentDate = document.querySelector("#current-date");
 currentDate.innerHTML = `${day}, ${month} ${date}, ${year}, ${time}`;
 
+function getForecast(coordinates) {
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let tempDisplay = document.querySelector("#current-temp");
   fahrenheitTemp = response.data.main.temp;
   tempDisplay.innerHTML = `${Math.round(fahrenheitTemp)}`;
+
+  getForecast(response.data.coord);
 }
 
 function displayConditions(response) {
@@ -70,6 +123,7 @@ function newCityWeather(event) {
   let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=${units}&appid=${apiKey}`;
   axios.get(apiURL).then(displayTemperature);
   axios.get(apiURL).then(displayConditions);
+  axios.get(apiURL).then(getForecast);
 }
 
 function showPositionWeather(position) {
